39f1a580106b7301519737beda25bf2a1188e00e
Sohil Lochan <sohil09956@gmail.com>
2024-08-31 13:50:34 +0530

    completed async function await usage by calling a Test API

---
+++ async in  js/script.js
@@ -0,0 +1,25 @@
+console.log(1);
+console.log(2);
+
+/*let res = fetch("https://jsonplaceholder.typicode.com/todos/1").then((res)=>{
+    return res.json();
+}).then((a)=>{
+    console.log(a)
+}).catch((error)=>{
+    console.log("User returned Error: ",error);
+});*/
+
+async function fetchData(){
+    try{
+        let data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        let real_data = await data.json();
+    }
+    catch(error){
+        console.log("Error Caught: ",error);
+    } finally{
+        console.log("Function Executed Successfully");
+    }
+
+}
+fetchData();
+console.log(3);
